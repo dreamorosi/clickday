5701051adcf2e70870b62e692a64ad5fdca59f17
Andrea Amorosi <dreamorosi@gmail.com>
2017-02-13 03:57:48 +0100

    Removed legacy assets

--- assets/js_legacy/experience.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	$('#subNavItems .navbar-left li a').click(function(e){
-		e.preventDefault();
-		$('html, body').animate({
-			scrollTop: $($(this).attr('href')).offset().top-200
-		}, 1500);
-	});
-});
