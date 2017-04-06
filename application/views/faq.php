9c9647bbbecf8bd2d811bac65ae2e910daaeb651
Andrea Amorosi <dreamorosi@gmail.com>
2017-04-06 16:27:14 +0200

    :books: Updated FAQs

--- application/views/faq.php
+++ application/views/faq.php
@@ -27,39 +27,76 @@
 					<div class="col-md-12">
 						<h2 id="clickDay2" class="text-uppercase">Informazioni Generali</h2>
 						<ul class="listF list-unstyled">
-							<li><span class="listFnumber">1</span><a role="button" data-toggle="collapse" href="#col1" aria-expanded="false">Quando ci sarà il Click Day?</a><div id="col1" class="collapse">Il giorno del Click Day sarà comunicato dall’INAIL a partire dal 12 giugno 2017, plausibilmente in una data compresa nelle due settimane successive. Vi aggiorneremo costantemente.</div></li>
-							<li><span class="listFnumber">2</span><a role="button" data-toggle="collapse" href="#col2" aria-expanded="false">Dove sarà realizzato il Click Day?</a><div id="col2" class="collapse">Da nessuna parte! Potrai fare il tuo click comodamente da casa!</div></li>
-							<li><span class="listFnumber">3</span><a role="button" data-toggle="collapse" href="#col3" aria-expanded="false">Quanto tempo dovrò impiegare per fare la procedura del click?</a><div id="col3" class="collapse">
-                                    Circa 5 minuti ma ti chiederemo di essere comunque seduto davanti al tuo PC 30 minuti prima dell’orario che vi comunicheremo.
-							</div></li>
-							<li><span class="listFnumber">4</span><a role="button" data-toggle="collapse" href="#col4" aria-expanded="false">Cosa c’entra il mio click con l’INAIL?</a><div id="col4" class="collapse">
-								Nulla. I cliccatori non hanno alcun tipo di relazione diretta con l’INAIL, ma solo con ATS - CONSULENTI ASSOCIATI S.r.l. in qualità di soggetto organizzatore del progetto. L’INAIL è l’Ente Pubblicoche si occupa di tutelare le vittime degli infortuni sul lavoro e che mette a disposizione delle imprese dei contributi finanziari per progetti di miglioramento dei livelli di salute e sicurezza nei luoghi di lavoro. Le aziende che sono interessate ad ottenere tali contributi si rivolgono a degli studi di consulenza – in questo caso ATS – Consulenti Associati – per svolgere tutte le pratiche per ottenerli, compreso l’invio della domanda tramite un click.
-							</div></li>
-							<li><span class="listFnumber">5</span><a role="button" data-toggle="collapse" href="#col5" aria-expanded="false">Cosa devo fare il giorno del Click Day?</a><div id="col5" class="collapse">
-								Accendere il tuo pc, seguire le istruzioni ed il video tutorial che ti avremo inviato via mail, fare click ed infine inviare lo screenshot/stamp della schermata d’invio generata dall’INAIL, il tutto loggandosi con le proprie credenziali sul portale clickdayats.it, attraverso la sezione “screenshots”.
-							</div></li>
-							<li><span class="listFnumber">6</span><a role="button" data-toggle="collapse" href="#col6" aria-expanded="false">Quando riceverò il mio compenso?</a><div id="col6" class="collapse">
-								Riceverai il tuo compenso, a mezzo di bonifico bancario, entro 30 giorni dalla pubblicazione degli elenchi cronologici dell’INAIL che individuano i progetti ammissibili a contributo. Tali elenchi saranno pubblicati entro 7 giorni dalla data del Clickday.
-							</div></li>
-							<li><span class="listFnumber">7</span><a role="button" data-toggle="collapse" href="#col7" aria-expanded="false">Posso partecipare anche se ho la partita IVA?</a><div id="col7" class="collapse">
-								Certo! Anziché un contratto di collaborazione occasionale, il cliccatore in possesso di p.IVA emetterà regolare fattura.
-							</div></li>
-							<li><span class="listFnumber">8</span><a role="button" data-toggle="collapse" href="#col8" aria-expanded="false">Posso visionare il contratto di collaborazione?</a><div id="col8" class="collapse">
-								Certo! Lo trovi a questo <a href="<? echo base_url('contratto.pdf'); ?>" target="_blank">link</a>!
-							</div></li>
-							<li><span class="listFnumber">9</span><a role="button" data-toggle="collapse" href="#col9" aria-expanded="false">Chi sono i Click Master?</a><div id="col9" class="collapse">
-								I Click Master sono i tutor dei cliccatori.<br>
-								Ogni Click Master gestisce un determinato numero di cliccatori inviando loro le comunicazioni relative al progetto (come iscriversi, come funziona il Click Day, quale giorno avverrà il click, ecc) e rispondendo alle loro eventuali domande.
-							</div></li>
-							<li><span class="listFnumber">10</span><a role="button" data-toggle="collapse" href="#col10" aria-expanded="false">Cosa succede se non hai un Click Master?</a><div id="col10" class="collapse">
-								Se ti iscrivi direttamente dal sito senza avere un codice Click Master, non preoccuparti, ti verrà assegnato in automatico al termine della registrazione.
-							</div></li>
-							<li><span class="listFnumber">11</span><a role="button" data-toggle="collapse" href="#col11" aria-expanded="false">Cosa succede se mi iscrivo e poi non faccio il click il giorno del Click Day?</a><div id="col11" class="collapse">
-								Non succede nulla. Non sarai obbligato in nessun modo a fare il click anche a seguito della tua iscrizione.<br>Nel caso in cui decidessi di non partecipare, ti chiediamo solo la cortesia di comunicarlo al tuo Click Master, lo potrai fare direttamente dal sistema di messaggistica presente nella tua area personale.
-							</div></li>
-							<li><span class="listFnumber">12</span><a role="button" data-toggle="collapse" href="#col12" aria-expanded="false">Dopo aver effettuato la registrazione a Click Day cosa devo fare?</a><div id="col12" class="collapse">
-								Una volta effettuata e confermata l'iscrizione, non dovrai fare altro che aspettare le comunicazioni del nostro Staff.<br>Il giorno e l’orario esatto, infatti, saranno comunicati dall’INAIL a partire dal 12 giugno 2017, plausibilmente in una data compresa nelle due settimane successive. Vi aggiorneremo costantemente.
-							</div></li>
+							<li>
+								<span class="listFnumber">1</span>
+								<a role="button" data-toggle="collapse" href="#col1" aria-expanded="false">Quando ci sarà il Click Day?</a>
+								<div id="col1" class="collapse">Il giorno del Click Day sarà comunicato dall’INAIL a partire dal 12 giugno 2017, plausibilmente in una data compresa nelle due settimane successive. Vi aggiorneremo costantemente.</div>
+							</li>
+							<li>
+								<span class="listFnumber">2</span>
+								<a role="button" data-toggle="collapse" href="#col2" aria-expanded="false">Dove sarà realizzato il Click Day?</a>
+								<div id="col2" class="collapse">Da nessuna parte! Potrai fare il tuo click comodamente da casa!</div>
+							</li>
+							<li>
+								<span class="listFnumber">3</span>
+								<a role="button" data-toggle="collapse" href="#col3" aria-expanded="false">Quanto tempo dovrò impiegare per fare la procedura del click?</a>
+								<div id="col3" class="collapse">Circa 5 minuti ma ti chiederemo di essere comunque seduto davanti al tuo PC 30 minuti prima dell’orario che vi comunicheremo.</div>
+							</li>
+							<li>
+								<span class="listFnumber">4</span>
+								<a role="button" data-toggle="collapse" href="#col4" aria-expanded="false">Cosa c’entra il mio click con l’INAIL?</a>
+								<div id="col4" class="collapse">Nulla. I cliccatori non hanno alcun tipo di relazione diretta con l’INAIL, ma solo con ATS - CONSULENTI ASSOCIATI S.r.l. in qualità di soggetto organizzatore del progetto. L’INAIL è l’Ente Pubblicoche si occupa di tutelare le vittime degli infortuni sul lavoro e che mette a disposizione delle imprese dei contributi finanziari per progetti di miglioramento dei livelli di salute e sicurezza nei luoghi di lavoro. Le aziende che sono interessate ad ottenere tali contributi si rivolgono a degli studi di consulenza – in questo caso ATS – Consulenti Associati – per svolgere tutte le pratiche per ottenerli, compreso l’invio della domanda tramite un click.</div>
+							</li>
+							<li>
+								<span class="listFnumber">5</span>
+								<a role="button" data-toggle="collapse" href="#col5" aria-expanded="false">Cosa devo fare il giorno del Click Day?</a>
+								<div id="col5" class="collapse">Accendere il tuo pc, seguire le istruzioni ed il video tutorial che ti avremo inviato via mail, fare click ed infine inviare lo screenshot/stamp della schermata d’invio generata dall’INAIL, il tutto loggandosi con le proprie credenziali sul portale clickdayats.it, attraverso la sezione “screenshots”.</div>
+							</li>
+							<li>
+								<span class="listFnumber">6</span>
+								<a role="button" data-toggle="collapse" href="#col6" aria-expanded="false">Quando riceverò il mio compenso?</a>
+								<div id="col6" class="collapse">Riceverai il tuo compenso, a mezzo di bonifico bancario, entro 30 giorni dalla pubblicazione degli elenchi cronologici dell’INAIL che individuano i progetti ammissibili a contributo. Tali elenchi saranno pubblicati entro 7 giorni dalla data del Clickday.</div>
+							</li>
+							<li>
+								<span class="listFnumber">7</span>
+								<a role="button" data-toggle="collapse" href="#col7" aria-expanded="false">Posso partecipare anche se ho la partita IVA?</a>
+								<div id="col7" class="collapse">Certo! Anziché un contratto di collaborazione occasionale, il cliccatore in possesso di p.IVA emetterà regolare fattura.</div>
+							</li>
+							<li>
+								<span class="listFnumber">8</span>
+								<a role="button" data-toggle="collapse" href="#col8" aria-expanded="false">Posso visionare il contratto di collaborazione?</a>
+								<div id="col8" class="collapse">Certo! Lo trovi a questo <a href="<? echo base_url('contratto.pdf'); ?>" target="_blank">link</a>!</div>
+							</li>
+							<li>
+								<span class="listFnumber">9</span>
+								<a role="button" data-toggle="collapse" href="#col9" aria-expanded="false">Cosa devo fare con il contratto?</a>
+								<div id="col9" class="collapse">Scaricalo, avrà già i tuoi dati inseriti, e inoltracelo firmato dall’apposita funzione di upload presente nella tua area personale entro 5 gironi dall’Iscrizione. Ti ricordiamo che è per una tua maggiore tutela.</div>
+							</li>
+							<li>
+								<span class="listFnumber">10</span>
+								<a role="button" data-toggle="collapse" href="#col10" aria-expanded="false">Chi sono i Click Master?</a>
+								<div id="col10" class="collapse">I Click Master sono i tutor dei cliccatori.<br>Ogni Click Master gestisce un determinato numero di cliccatori inviando loro le comunicazioni relative al progetto (come iscriversi, come funziona il Click Day, quale giorno avverrà il click, ecc) e rispondendo alle loro eventuali domande.</div>
+							</li>
+							<li>
+								<span class="listFnumber">11</span>
+								<a role="button" data-toggle="collapse" href="#col11" aria-expanded="false">Cosa succede se non hai un Click Master?</a>
+								<div id="col11" class="collapse">Se ti iscrivi direttamente dal sito senza avere un codice Click Master, non preoccuparti, ti verrà assegnato in automatico al termine della registrazione.</div>
+							</li>
+							<li>
+								<span class="listFnumber">12</span>
+								<a role="button" data-toggle="collapse" href="#col12" aria-expanded="false">Cosa succede se mi iscrivo e poi non faccio il click il giorno del Click Day?</a>
+								<div id="col12" class="collapse">Non succede nulla. Non sarai obbligato in nessun modo a fare il click anche a seguito della tua iscrizione.<br>Nel caso in cui decidessi di non partecipare, ti chiediamo solo la cortesia di comunicarlo al tuo Click Master, lo potrai fare direttamente dal sistema di messaggistica presente nella tua area personale.</div>
+							</li>
+							<li>
+								<span class="listFnumber">13</span>
+								<a role="button" data-toggle="collapse" href="#col13" aria-expanded="false">Dopo aver effettuato la registrazione a Click Day cosa devo fare?</a>
+								<div id="col13" class="collapse">Una volta effettuata e confermata l'iscrizione, non dovrai fare altro che aspettare le comunicazioni del nostro Staff.<br>Il giorno e l’orario esatto, infatti, saranno comunicati dall’INAIL a partire dal 12 giugno 2017, plausibilmente in una data compresa nelle due settimane successive. Vi aggiorneremo costantemente.</div>
+							</li>
+							<li>
+								<span class="listFnumber">14</span>
+								<a role="button" data-toggle="collapse" href="#col14" aria-expanded="false">Come faccio a cancellare il mio account ?</a>
+								<div id="col14" class="collapse">Ti sei iscritto a Click Day 2017 ma hai cambiato idea e non vuoi più partecipare?<br /><br />Nessun problema! Puoi inviare una mail al tuo Click Master chiedendo l'annullamento dell'iscrizione. Nel caso tu non avessi un Click Master potrai scrivere a <a href='mailto:info@clickdayats.it'>info@clickdayats.it</a>.</div>
+							</li>
 						</ul>
 					</div>
 				</div>
